21a4e738d2fe90fb2d54eb213b28230b6fa229b2
Ryan Hantak <rhantak@gmail.com>
2020-01-29 23:49:41 -0700

    Change base repo to work with google api

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -4,12 +4,14 @@ var router = express.Router();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
+const fetch = require("node-fetch");
 
 
 router.get('/', (request, response) => {
-  database('papers').select()
-    .then((papers) => {
-      response.status(200).json(papers);
+  fetch('https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=AIzaSyA9oPZa8kXUwBEuFRo5-uKUaxDMJhfbBXw')
+    .then((response) => response.json())
+    .then(result => {
+      response.status(200).json(result.results[0].geometry.location);
     })
     .catch((error) => {
       response.status(500).json({ error });
