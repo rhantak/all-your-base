c0516cb7cd425ed463b61808051e43d9342a92f4
Ryan Hantak <rhantak@gmail.com>
2020-01-29 23:51:24 -0700

    Add migration for users table

--- db/migrations/20190128225204_initial.js
+++ db/migrations/20190128225204_initial.js
@@ -1,19 +1,18 @@
 
 exports.up = function(knex) {
   return Promise.all([
-    // knex.schema.createTable('papers', function(table) {
-    //   table.increments('id').primary();
-    //   table.string('title');
-    //   table.string('author');
-    //
-    //   table.timestamps(true, true);
+    knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.string('api_key');
+
+      table.timestamps(true, true);
     })
   ])
 };
 
 
 exports.down = function(knex) {
-  // return Promise.all([
-  //   knex.schema.dropTable('papers')
+  return Promise.all([
+    knex.schema.dropTable('users')
   ]);
 }
