50a31fcb06aedc71a1029c0b5ac6fa2eff810a83
Ryan Hantak <rhantak@gmail.com>
2020-01-30 12:09:56 -0700

    Add daily key to location forecast

--- location_forecast.js
+++ location_forecast.js
@@ -1,11 +1,13 @@
 const CurrentForecast = require('./current_forecast')
 const HourlyForecast = require('./hourly_forecast')
+const DailyForecast = require('./daily_forecast')
 
 class LocationForecast {
   constructor(forecast_data, location){
     this.location = location
     this.currently = new CurrentForecast(forecast_data.currently)
     this.hourly = new HourlyForecast(forecast_data.hourly)
+    this.daily = new DailyForecast(forecast_data.daily)
   }
 }
 
