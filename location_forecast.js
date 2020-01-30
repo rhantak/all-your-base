bed007f207be9727463e9e4cd970a11f10d5c618
Ryan Hantak <rhantak@gmail.com>
2020-01-30 11:55:55 -0700

    Add hourly forecast class to location forecast

--- location_forecast.js
+++ location_forecast.js
@@ -1,9 +1,11 @@
 const CurrentForecast = require('./current_forecast')
+const HourlyForecast = require('./hourly_forecast')
 
 class LocationForecast {
   constructor(forecast_data, location){
     this.location = location
     this.currently = new CurrentForecast(forecast_data.currently)
+    this.hourly = new HourlyForecast(forecast_data.hourly)
   }
 }
 
